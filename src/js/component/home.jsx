f16ccaca0212d8a0fd7d962ab016ecf59e1e7785
romanmonfort <137888802+romanmonfort@users.noreply.github.com>
2023-11-13 18:02:52 +0000

    todo list

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -27,27 +27,42 @@ const Home = () => {
 
 
 	return (
-		<div className="text-center">
-			<h1>TODO</h1>
-			<div className="d-flex justify-content-center align-items-center">
-				<input
+		<div className="container-fluid">
+		<div className="row justify-content-center align-items-center" >
+		  <div className="col-md-4">
+			<div className="text-center card h-100">
+			  <p>todos</p>
+			  
+  
+			  <ul className="list-group mt-3 " >
+				<li className="list-group-item">
+				  <input
+					className="ms-5 border-none"
 					type="text"
 					onChange={(e) => setTarea(e.target.value)}
 					value={tarea}
 					onKeyDownCapture={Enter}
 					placeholder="Pone algo "
-				/>
-			</div>
-			<ul className="list-group mt-3">
+				  />
+				</li>
+  
 				{lista.map((tarea, index) => (
-					<li key={index} className="list-group-item">
-						{tarea}<button key={index} className="ms-5 btn btn-danger" onClick={Eliminar} >X</button>
-					</li>
+				  <li key={index} className=" list-group-item d-flex justify-content-between align-items-center">
+					<p>{tarea}</p>
+					<i class="fa-solid fa-x" style={{color: '#ff0000'}} key={index} onClick={Eliminar}></i>
+				  </li>
 				))}
-			</ul>
+			  </ul>
+  
+			  <div className="card-footer" >
+				<small className="text-body-secondary">Hay {lista.length} actividades por hacer</small>
+			  </div>
+			</div>
+		  </div>
 		</div>
+	  </div>
 
-
+      
 	);
 };
 
